5ef66e3c51060d2d21da751be46181381115219c
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2025-06-16 19:26:45 -0700

    variables

---
+++ script.js
@@ -0,0 +1,59 @@
+console.log("Script");
+// VARIABLES
+
+//Strings
+let firstName = "Samantha"; // assing the value
+let lastName = 'Jimenez';
+
+//numerical
+let age = 99;
+let mySize = 10;
+
+// boolean values
+let isStudent = false;
+
+console.log(firstName);
+console.log(age,mySize);
+
+//concatenation
+console.log("My name is " + firstName + " " + lastName);
+
+//Operations
+
+let num1 = 10; 
+let num2 = 2;
+
+let add = num1 + num2; 
+let subs = num1 - num2; 
+let div = num1/num2;
+let mult = num1 * num2;
+// subs,div, mult
+
+
+console.log("Add is: " + add);
+console.log("Subs is: " + subs);
+console.log("Div is: " + div);
+console.log("Mult is: " + mult);
+
+let a;
+const pi = 3.1416;
+let r = 3;
+
+a = pi * (r*r);
+console.log("The area of a circile with a radius of " + r + " is the " + a.toFixed(2));
+
+let aVar = 20;
+let otherVar = 30;
+
+console.log(aVar,otherVar);
+
+aVar = 50;
+otherVar = 60;
+
+console.log(aVar,otherVar);
+
+const weekDays = 5;
+
+//weekDays = 8; // the constant can't change
+
+console.log(weekDays);
